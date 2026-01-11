46fbaacf3e7799d7f85b2829e576f71c5ee6daaa
gitsydneyuser <nishant4msydney@gmail.com>
2026-01-11 17:39:30 +1100

    test commit message

--- src/playwright_basic.ts
+++ src/playwright_basic.ts
@@ -9,7 +9,7 @@ const browser : Browser = await chromium.launch({headless: false});
 const page = await browser.newPage();
 await page.goto("https://www.google.com/");
 
-await page.locator("#APjFqb").fill("Hello World");
+await page.locator("#APjFqb").fill("Hello World123");
 
 await page.locator("(//input[@name='btnK'])[2]").click();
 
